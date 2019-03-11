41be33935a19bbaa06e9845d5b7da429125afb43
Luka Galjot <luka.galjot@gmail.com>
2019-03-11 12:59:28 +0100

    Dodaj osnovne entitete

---
+++ entitete/src/main/java/Uporabnik.java
@@ -0,0 +1,14 @@
+public class Uporabnik {
+    private String name;
+
+    public Uporabnik() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ entitete/src/main/java/VprOdgObjekt.java
@@ -0,0 +1,23 @@
+public class VprOdgObjekt {
+    private String question;
+    private Object[] answers;
+
+    public VprOdgObjekt() {
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Object[] getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(Object[] answers) {
+        this.answers = answers;
+    }
+}
